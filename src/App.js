8b23472cd95efaf013dc1d53c255e03b920ca37b
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2023-12-02 22:47:39 -0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./categories.styles.scss";
+import Directory from "./components/directory/directory.component";
 const App = () => {
   const categories = [
     {
@@ -27,19 +27,7 @@ const App = () => {
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id }) => (
-        <div className="category-container" key={id}>
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h1>{title}</h1>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
