f4024c268b96c69f6d68c7786236de0034a9d5f4
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2023-12-18 23:27:56 -0500

    added the cart icon feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,10 +5,13 @@ import { CartContext } from "../../contexts/cart.context";
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  const { cartCount } = useContext(CartContext);
+  // let totalItemsInCart = 0;
+  // cartItems.forEach((item) => (totalItemsInCart += item.quantity));
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
